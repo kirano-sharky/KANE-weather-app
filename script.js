6458ba2e1f74a6a985ec082c8157039edfec266d
weeeeeeee <95114619+meA-sonWat@users.noreply.github.com>
2022-10-08 21:54:18 +0800

    undone

--- script.js
+++ script.js
@@ -31,8 +31,6 @@ function displayData (応答) {
     else {
         const 街 = document.querySelector('.街');
         街.innerText = `${応答.name}, ${応答.sys.country}`;
-        /*const 成功 = document.querySelector('.成功')
-        成功.textContent = '做的好';*/
 
         /*const today = new Date();
         const date = document.querySelector('.date');
@@ -55,3 +53,25 @@ function displayData (応答) {
     }
 }
 
+setInterval(()=>{
+    const 日 = document.querySelector(".時間 .日");
+    let 日期 = new Date();
+    let 小時 = 日期.getHours();
+    let 分鐘 = 日期.getMinutes();
+    let 秒 = 日期.getSeconds();
+    let 時段 = "AM";
+    if(小時 > 12){
+      時段 = "PM";
+      小時 = 小時 - 12;
+    }
+    if(秒 < 10){
+      秒 = "0" + 秒;
+    }
+    if(分鐘 < 10){
+      分鐘 = "0" + 分鐘;
+    }
+    if(小時 < 10){
+      小時 = "0" + 小時;
+    }
+    日.textContent = 小時 + ":" + 分鐘 + ":" + 秒 + " "+ 時段;
+  });
